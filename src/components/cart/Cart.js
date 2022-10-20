a5df36346a03a7c272732237b1277e99557a10bb
julianrodriguez2728 <julirdrg@gmail.com>
2022-10-20 20:22:36 -0300

    actualizado

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -2,7 +2,7 @@ import { useContext } from "react"
 import { CartContext } from "../../context/CartContext"
 
 const Cart = ()=>{
-    const {cart}= useContext(CartContext);
+    const {cart,  deleteAll}= useContext(CartContext);
 
     return(
         <div>
@@ -10,14 +10,16 @@ const Cart = ()=>{
                 cart.map ((prod)=>{
                     return(
                            <div 
-                           key={prod.id}>
+                           key={prod.id} 
+                           className="contenedor-carrito1">
                         <img src={prod.img} alt={prod.title} />
-                        <div className="contenedor-carrito">
+                        <div className="contenedor-carrito2">
                             <h2>{prod.title}</h2>
                             <h3>$ {prod.price}</h3>
                             <h3>cantidad: {prod.cantidad}</h3>
                             <h3>subtotal: ${prod.price*prod.cantidad}</h3>
                         </div>
+                        <button onClick={deleteAll} className="borrar">Borrar todo</button>
                     </div>
                        ) 
                 })
